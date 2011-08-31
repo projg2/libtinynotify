ec49840ee0c4c784cacdb13075940e127e235b5f
Michał Górny <mgorny@gentoo.org>
2011-08-31 18:18:45 +0200

    Introduce feature check defines.

--- lib/tinynotify.h
+++ lib/tinynotify.h
@@ -7,6 +7,33 @@
 #define _TINYNOTIFY_H
 
 /**
+ * SECTION: NotifyFeatures
+ * @short_description: defines for simple feature checks
+ * @include: tinynotify.h
+ *
+ * As of 0.1.1, libtinynotify provides a set of macro declarations listing
+ * available features. This way, one doesn't have to depend on random version
+ * numbers nor perform configure checks -- it is enough to use \#ifdef with one
+ * of the predefined names.
+ */
+
+/**
+ * LIBTINYNOTIFY_HAS_EVENT_API
+ *
+ * Denotes that libtinynotify has event API with notify_session_dispatch()
+ * method and relevant types and constants.
+ */
+#define LIBTINYNOTIFY_HAS_EVENT_API 1
+
+/**
+ * LIBTINYNOTIFY_HAS_CLOSE_CALLBACK
+ *
+ * Denotes that libtinynotify has notification_bind_close_callback() function
+ * along with relevant types and constants.
+ */
+#define LIBTINYNOTIFY_HAS_CLOSE_CALLBACK 1
+
+/**
  * SECTION: NotifySession
  * @short_description: API to deal with libtinynotify sessions
  * @include: tinynotify.h
