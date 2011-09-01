597fd2dace19347d96aab8f26c0fdf2db6fffd52
Michał Górny <mgorny@gentoo.org>
2011-09-01 12:44:09 +0200

    Move struct _notification into notification_.h.

---
+++ lib/notification_.h
@@ -0,0 +1,32 @@
+/* libtinynotify -- Notification instance
+ * (c) 2011 Michał Górny
+ * 2-clause BSD-licensed
+ */
+
+#pragma once
+#ifndef _TINYNOTIFY_NOTIFICATION__H
+#define _TINYNOTIFY_NOTIFICATION__H
+
+#include <dbus/dbus.h>
+
+#include "notification.h"
+#include "event.h"
+
+struct _notification {
+	char* summary;
+	char* body;
+	int formatting;
+
+	NotificationCloseCallback close_callback;
+	void* close_data;
+
+	dbus_int32_t expire_timeout;
+	NotificationUrgency urgency;
+	char* category;
+
+	char* app_icon;
+
+	dbus_uint32_t message_id;
+};
+
+#endif /*_TINYNOTIFY_NOTIFICATION__H*/
--- lib/tinynotify.c
+++ lib/tinynotify.c
@@ -6,6 +6,7 @@
 #include "config.h"
 #include "common_.h"
 #include "session_.h"
+#include "notification_.h"
 
 #include "error.h"
 #include "notification.h"
@@ -23,23 +24,6 @@
 #ifdef HAVE_LIBSTRL
 #	include <strl.h>
 #endif
-
-struct _notification {
-	char* summary;
-	char* body;
-	int formatting;
-
-	NotificationCloseCallback close_callback;
-	void* close_data;
-
-	dbus_int32_t expire_timeout;
-	NotificationUrgency urgency;
-	char* category;
-
-	char* app_icon;
-
-	dbus_uint32_t message_id;
-};
 
 const NotifyError NOTIFY_ERROR_NO_ERROR = NULL;
 
