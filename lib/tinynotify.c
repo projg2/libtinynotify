aa05964fd6d8da698f618c8ae872c1b31df72ca6
Michał Górny <mgorny@gentoo.org>
2011-09-01 13:16:06 +0200

    Move Notification constr/destr to notification unit.

--- lib/tinynotify.c
+++ lib/tinynotify.c
@@ -25,6 +25,8 @@
 #	include <strl.h>
 #endif
 
+static const dbus_uint32_t NOTIFICATION_NO_NOTIFICATION_ID = 0;
+
 static void _notify_session_add_notification(NotifySession s, Notification n) {
 	struct _notification_list *nl;
 
@@ -54,44 +56,6 @@ static void _notify_session_add_notification(NotifySession s, Notification n) {
 	nl->n = n;
 	nl->next = s->notifications;
 	s->notifications = nl;
-}
-
-static const dbus_uint32_t NOTIFICATION_NO_NOTIFICATION_ID = 0;
-
-Notification notification_new_unformatted(const char* summary, const char* body) {
-	Notification n;
-
-	assert(summary);
-
-	_mem_assert(n = malloc(sizeof(*n)));
-	/* can't use notification_set_summary() here because it has to free sth */
-	_mem_assert(n->summary = strdup(summary));
-	n->body = NULL;
-	n->category = NULL;
-	n->app_icon = NULL;
-	n->message_id = NOTIFICATION_NO_NOTIFICATION_ID;
-
-	notification_set_body(n, body);
-	notification_set_formatting(n, 0);
-	notification_set_expire_timeout(n, NOTIFICATION_DEFAULT_EXPIRE_TIMEOUT);
-	notification_set_urgency(n, NOTIFICATION_NO_URGENCY);
-	notification_bind_close_callback(n, NOTIFICATION_NO_CLOSE_CALLBACK, NULL);
-	return n;
-}
-
-Notification notification_new(const char* summary, const char* body) {
-	Notification n = notification_new_unformatted(summary, body);
-	notification_set_formatting(n, 1);
-	return n;
-}
-
-void notification_free(Notification n) {
-	free(n->summary);
-	if (n->body)
-		free(n->body);
-	if (n->app_icon)
-		free(n->app_icon);
-	free(n);
 }
 
 void notification_set_app_icon(Notification n, const char* app_icon) {
